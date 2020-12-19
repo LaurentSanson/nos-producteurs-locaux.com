76ee801b206800aee6c15c4c28e5b51964685e7a
LaurentSanson <56510710+LaurentSanson@users.noreply.github.com>
2020-12-19 19:20:29 +0100

    feat: login and logout (#34)
    Co-authored-by: LaurentSanson <laurent.sanson29@gmail.com>

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Customer;
+use App\Entity\Producer;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Uid\Uuid;
+
+class UserFixtures extends Fixture
+{
+
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+
+    /**
+     * UserFixtures constructor.
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $producer = new Producer();
+        $producer->setId(Uuid::v4());
+        $producer->setPassword($this->userPasswordEncoder->encodePassword($producer, 'password'));
+        $producer->setFirstname('Jane');
+        $producer->setLastname('Doe');
+        $producer->setEmail('producer@email.com');
+        $manager->persist($producer);
+
+        $customer = new Customer();
+        $customer->setId(Uuid::v4());
+        $customer->setPassword($this->userPasswordEncoder->encodePassword($customer, 'password'));
+        $customer->setFirstname('John');
+        $customer->setLastname('Doe');
+        $customer->setEmail('customer@email.com');
+        $manager->persist($customer);
+
+        $manager->flush();
+    }
+}
