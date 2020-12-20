ab82de335c9c2350754fd10d927b31bc676ee07c
LaurentSanson <laurent.sanson29@gmail.com>
2020-12-20 13:54:54 +0100

    fix: fix farm entity

--- src/Entity/Producer.php
+++ src/Entity/Producer.php
@@ -15,30 +15,9 @@ class Producer extends User
 {
     public const ROLE = "producer";
 
-    /**
-     * @ORM\OneToOne(targetEntity=Farm::class, mappedBy="producer", cascade={"persist", "remove"})
-     */
-    private $farm;
-
     public function getRoles(): array
     {
         return ['ROLE_PRODUCER'];
     }
 
-    public function getFarm(): ?Farm
-    {
-        return $this->farm;
-    }
-
-    public function setFarm(Farm $farm): self
-    {
-        // set the owning side of the relation if necessary
-        if ($farm->getProducer() !== $this) {
-            $farm->setProducer($this);
-        }
-
-        $this->farm = $farm;
-
-        return $this;
-    }
 }
