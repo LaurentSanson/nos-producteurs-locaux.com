10efeee2ddf05cfdc47d43420ceac895e9699d30
LaurentSanson <56510710+LaurentSanson@users.noreply.github.com>
2020-12-29 17:59:40 +0100

    feat: update product stock (#39)
    Co-authored-by: LaurentSanson <laurent.sanson29@gmail.com>

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Form\StockType;
 use App\Repository\ProductRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -98,5 +99,31 @@ class ProductController extends AbstractController
         );
 
         return $this->redirectToRoute('product_index');
+    }
+
+    /**
+     * @Route("/{id}/stock", name="product_stock")
+     * @param Product $product
+     * @param Request $request
+     * @return Response
+     * @IsGranted("update", subject="product")
+     */
+    public function stock(Product $product, Request $request): Response
+    {
+        $form = $this->createForm(StockType::class, $product)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash(
+                "success",
+                "Le stock de votre produit a été modifié avec succès"
+            );
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('ui/product/stock.html.twig', [
+            'productForm' => $form->createView(),
+        ]);
     }
 }
