4d73beb731005eb1f56c0f07645f01105a510965
LaurentSanson <56510710+LaurentSanson@users.noreply.github.com>
2021-01-02 10:35:58 +0100

    feat: cart (#42)
    Co-authored-by: LaurentSanson <laurent.sanson29@gmail.com>

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Address;
 use App\Entity\Customer;
 use App\Entity\Farm;
+use App\Entity\Position;
 use App\Entity\Price;
 use App\Entity\Producer;
 use App\Entity\Product;
@@ -12,6 +14,8 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Uid\Uuid;
+
+use function Sodium\add;
 
 class ProductFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -25,6 +29,15 @@ class ProductFixtures extends Fixture implements DependentFixtureInterface
 
         /** @var Farm $farm */
         foreach ($farms as $farm) {
+            $position = new Position();
+            $position->setLatitude(41.5);
+            $position->setLongitude(7.5);
+            $address = new Address();
+            $address->setAddress("14 impasse du chant des oiseaux");
+            $address->setPostCode("44210");
+            $address->setCity("Pornic");
+            $address->setPosition($position);
+            $farm->setAddress($address);
             for ($i = 1; $i <= 10; $i++) {
                 $product = new Product();
                 $product->setId(Uuid::v4());
