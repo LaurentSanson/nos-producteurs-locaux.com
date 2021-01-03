31e2e9748cb2d08ffdd58e913d3751693d7b171b
LaurentSanson <laurent.sanson29@gmail.com>
2021-01-03 09:45:05 +0100

    feat: create order, cancel and history

---
+++ src/Security/Voter/OrderVoter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Customer;
+use App\Entity\Order;
+use App\Entity\Producer;
+use App\Entity\Product;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+/**
+ * Class OrderVoter
+ * @package App\Security\Voter
+ */
+class OrderVoter extends Voter
+{
+
+    public const CANCEL = "cancel";
+
+    /**
+     * @@inheritDoc
+     */
+    protected function supports(string $attribute, $subject): bool
+    {
+        return in_array($attribute, [self::CANCEL]) && $subject instanceof Order;
+    }
+
+    /**
+     * @@inheritDoc
+     */
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof Customer) {
+            return false;
+        }
+
+        /** @var Order $subject */
+        return $subject->getState() === "created";
+    }
+}
