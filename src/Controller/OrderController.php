f12182c55e0599c0143660059487e527b1119789
LaurentSanson <56510710+LaurentSanson@users.noreply.github.com>
2021-01-03 09:47:18 +0100

    feat: create order, cancel and history (#43)
    Co-authored-by: LaurentSanson <laurent.sanson29@gmail.com>

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CartItem;
+use App\Entity\Order;
+use App\Entity\OrderLine;
+use App\Repository\OrderRepository;
+use DateTimeImmutable;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * Class OrderController
+ * @package App\Controller
+ * @Route("/order")
+ */
+class OrderController extends AbstractController
+{
+    /**
+     * @Route("/create", name="order_create")
+     * @IsGranted("ROLE_CUSTOMER")
+     */
+    public function create(): RedirectResponse
+    {
+        $order = new Order();
+        $order->setId(Uuid::v4());
+        $order->setCustomer($this->getUser());
+        /** @var CartItem $cartItem */
+        foreach ($this->getUser()->getCart() as $cartItem) {
+            $line = new OrderLine();
+            $line->setId(Uuid::v4());
+            $line->setOrder($order);
+            $line->setQuantity($cartItem->getQuantity());
+            $line->setProduct($cartItem->getProduct());
+            $line->setPrice($cartItem->getProduct()->getPrice());
+
+            $order->getLines()->add($line);
+        }
+        $this->getUser()->getCart()->clear();
+        $this->getDoctrine()->getManager()->persist($order);
+        $this->getDoctrine()->getManager()->flush();
+        return $this->redirectToRoute('index');
+    }
+
+    /**
+     * @Route("/history", name="order_history")
+     * @param OrderRepository $orderRepository
+     * @return Response
+     * @IsGranted("ROLE_CUSTOMER")
+     */
+    public function history(OrderRepository $orderRepository): Response
+    {
+        return $this->render("ui/order/history.html.twig", [
+            "orders" => $orderRepository->findByCustomer($this->getUser())
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/cancel", name="order_cancel")
+     * @param Order $order
+     * @return RedirectResponse
+     * @IsGranted("cancel", subject="order")
+     */
+    public function cancel(Order $order): RedirectResponse
+    {
+        $order->setState("canceled");
+        $order->setCanceledAt(new DateTimeImmutable());
+        $this->getDoctrine()->getManager()->flush();
+        return $this->redirectToRoute("order_history");
+    }
+}
---
+++ tests/OrderTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Farm;
+use App\Entity\Order;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Generator;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * Class OrderTest
+ * @package App\Tests
+ */
+class OrderTest extends WebTestCase
+{
+    use AuthenticationTrait;
+
+    public function testSuccessfulCreateOrder(): void
+    {
+        $client = static::createAuthenticatedClient("customer@email.com");
+
+        /** @var RouterInterface $router */
+        $router = $client->getContainer()->get('router');
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $product = $entityManager->getRepository(Product::class)->findOneBy([]);
+
+        $client->request(Request::METHOD_GET, $router->generate("cart_add", [
+            "id" => $product->getId()
+        ]));
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->request(Request::METHOD_GET, $router->generate("order_create"));
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+    }
+
+    public function testSuccessfulCancelOrder(): void
+    {
+        $client = static::createAuthenticatedClient("customer@email.com");
+
+        /** @var RouterInterface $router */
+        $router = $client->getContainer()->get('router');
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $product = $entityManager->getRepository(Product::class)->findOneBy([]);
+
+        $client->request(Request::METHOD_GET, $router->generate("cart_add", [
+            "id" => $product->getId()
+        ]));
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->request(Request::METHOD_GET, $router->generate("order_create"));
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $order = $entityManager->getRepository(Order::class)->findOneBy(["state" => "created"]);
+
+
+        $client->request(Request::METHOD_GET, $router->generate("order_cancel", [
+            "id" => $order->getId()
+        ]));
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+    }
+}
